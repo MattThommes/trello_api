f3f172b0e984413dbd572e625d998c6fddbf0156
Matt Thommes <matt@activecampaign.com>
2014-07-19 13:58:10 -0500

    tweaks to script. also included gitignore and package.json file

--- cards_recent-list.js
+++ cards_recent-list.js
@@ -1,12 +1,20 @@
 var Trello = require("node-trello");
 var t = new Trello("", "");
 
-t.get("/1/lists/529cabd4391c4ddf3f0050ae/cards", function(err, data) {
+var list_id = "529cabd4391c4ddf3f0050ae";
+var list_name = "";
+
+// get list info.
+t.get("/1/lists/" + list_id, function(err, data) {
+	list_name = data.name;
+});
+
+// get card info.
+t.get("/1/lists/" + list_id + "/cards", function(err, data) {
   if (err) throw err;
-	console.log("\n'Recent' cards:\n");
+	console.log("\n'" + list_name + "' cards:\n");
 	var counter = 1;
   for (var i in data) {
-//console.log(data[i].labels);
 		var label_arr = [];
 		for (var j in data[i].labels) {
 			if (typeof(data[i].labels[j].name) != "undefined") {
@@ -14,21 +22,8 @@ t.get("/1/lists/529cabd4391c4ddf3f0050ae/cards", function(err, data) {
 			}
 		}
 		var label_str = label_arr.join(", ");
-  	var str = counter + ". " + label_str + " [" + data[i].name + "](" + data[i].shortUrl + ")\n";
-  	console.log(str);
-  	counter++;
-  }
+		var str = counter + ". " + label_str + " [" + data[i].name + "](" + data[i].shortUrl + ")\n";
+		console.log(str);
+		counter++;
+	}
 });
-
-/*
-t.get("/1/members/me", function(err, data) {
-  if (err) throw err;
-  console.log(data);
-});
-
-// URL arguments are passed in as an object.
-t.get("/1/members/me", { cards: "open" }, function(err, data) {
-  if (err) throw err;
-  console.log(data);
-});
-*/
